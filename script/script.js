5f3206587f56990be4ced29da086d592db5ac7bb
yolandamatiwane <mbalentle.matiwane21@gmail.com>
2024-07-01 10:12:44 +0200

    added styling

--- script/script.js
+++ script/script.js
@@ -3,7 +3,13 @@ let inpH = document.querySelector('#height')
 let btn = document.querySelector('button')
 let spn = document.querySelector('#span')
 let ans = document.querySelector('#ans')
+let clear = document.querySelector('#clear')
 
 btn.addEventListener('click',()=>{
     ans.value = eval(`${spn.innerText}*${inpB.value}*${inpH.value}`)
+})
+clear.addEventListener('click',()=>{
+    ans.value = ''
+    inpB.value = ''
+    inpH.value = ''
 })
